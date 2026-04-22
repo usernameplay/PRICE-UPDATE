4c3ce6db302e79cf339565681bac48ae0b0962e1
usernameplay <132442887+usernameplay@users.noreply.github.com>
2026-04-23 00:53:24 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from datetime import datetime
 import json
 import requests
-import pytz
 
 app = Flask(__name__)
 CORS(app)
@@ -11,9 +9,6 @@ CORS(app)
 UPSTASH_URL = "https://pleasing-hagfish-93543.upstash.io"
 UPSTASH_TOKEN = "gQAAAAAAAW1nAAIgcDJjMWU5NzE2NTY0OGE0MWM4YWZhYjA5NDU1MTRhYWM2OA"
 headers = {"Authorization": f"Bearer {UPSTASH_TOKEN}", "Content-Type": "application/json"}
-
-def get_now():
-    return datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%d %b, %I:%M %p")
 
 @app.route('/api/veg', methods=['GET'])
 def fetch_all():
@@ -28,19 +23,13 @@ def fetch_all():
 def save_all():
     try:
         items = request.json
-        last_sync = get_now()
-        for i in items:
-            i['last_updated'] = last_sync
-            
+        # Frontend അയക്കുന്ന സമയം തന്നെ ഡാറ്റാബേസിൽ സേവ് ചെയ്യുന്നു
         requests.post(f"{UPSTASH_URL}/set/vegetables", headers=headers, data=json.dumps(items))
-        return jsonify({"status": "success", "time": last_sync})
+        return jsonify({"status": "success"})
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
-# Vercel-ന് ആവശ്യമായ ഭാഗം
+# Vercel integration
 def handler(event, context):
     return app(event, context)
-
-if __name__ == '__main__':
-    app.run(debug=True)
     
